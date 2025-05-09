a739360585432e9f3ad636c23713b14038ef6703
MfJason-320 <zzjay31@gmail.com>
2025-05-09 20:41:13 +0530

    Fix runtime issue with correct vercel.json

--- api/update.py
+++ api/update.py
@@ -1,11 +1,11 @@
 from app.storage import init_db
 from app.nav_fetcher import fetch_and_store_schemes, fetch_daily_nav
 
-def handler(_, response):  # underscore replaces unused 'request'
+def handler(request, response):
     try:
         init_db()
         fetch_and_store_schemes()
         fetch_daily_nav()
-        return response.json({"status": "success", "message": "NAV data updated."})
+        return response.json({"status": "success", "message": "NAV updated."})
     except Exception as e:
         return response.json({"status": "error", "message": str(e)})
